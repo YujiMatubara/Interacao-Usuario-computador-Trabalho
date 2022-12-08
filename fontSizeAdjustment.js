f871edda05cd7779e0ceec0654858cb731f1af08
lucasyamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2022-12-08 00:59:17 -0300

    Remove console log

--- fontSizeAdjustment.js
+++ fontSizeAdjustment.js
@@ -2,7 +2,7 @@ function fontSizeAdjustment(lowerLimit = 16, upperLimit = 32) {
   const increaseButton = document.getElementById("font-size-increase");
   const decreaseButton = document.getElementById("font-size-decrease");
   const texts = document.getElementsByClassName("font-size");
-  console.log(texts);
+  
   increaseButton.addEventListener("click", () => {
     for (let i = 0; i < texts.length; i++) {
       const currentFontSize = Number(
